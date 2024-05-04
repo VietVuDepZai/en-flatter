048d86cce37bf452c156d7063fbcd9e0262ad18f
vietmrvu <viet.mrvu@gmail.com>
2024-05-04 16:18:20 +0700

    one

--- depression_Helper/settings.py
+++ depression_Helper/settings.py
@@ -155,7 +155,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR  / 'static/images'
-STATIC_ROOT = BASE_DIR / 'staticfiles_build'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
 
 LOGOUT_REDIRECT_URL = '/login'
 
